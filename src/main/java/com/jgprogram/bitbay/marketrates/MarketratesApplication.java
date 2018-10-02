4bad56d41f256629655159cce81e2de2948a8e83
Jacek Gzel <jacek.gzel@gmail.com>
2018-10-02 18:54:46 +0200

    Init project

---
+++ src/main/java/com/jgprogram/bitbay/marketrates/MarketratesApplication.java
@@ -0,0 +1,12 @@
+package com.jgprogram.bitbay.marketrates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketratesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketratesApplication.class, args);
+	}
+}
